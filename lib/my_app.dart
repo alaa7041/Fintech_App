81f976a741bf61afcc17d0d38a1a2b8e65c9d689
alaa <a.magdy2407@gmail.com>
2025-11-21 20:38:13 +0200

    add fonts

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/helpers/constants.dart';
+import 'core/routing/route_manager.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: ScreenUtilInit(
+        designSize: const Size(390, 812),
+        builder: (_, child) {
+          return  MaterialApp(
+              useInheritedMediaQuery: true,
+              navigatorKey: navigatorKey,
+              debugShowCheckedModeBanner: false,
+              title: appName,
+              theme: ThemeData(
+                  fontFamily: 'Lato',
+                  scaffoldBackgroundColor: const Color(0xFFFFFFFE)),
+              home: Scaffold(),
+            )
+          ;
+        },
       ),
     );
   }
