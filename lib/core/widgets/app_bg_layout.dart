5e1a054a0d8adbaeb0c160b2a7d9aa990142c88a
alaa <a.magdy2407@gmail.com>
2025-11-29 11:56:26 +0200

    add custo app bg layout 🎨

---
+++ lib/core/widgets/app_bg_layout.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import '../helpers/utils.dart';
+
+class AppBackgroundLayout extends StatelessWidget {
+  final Widget? appBar;
+  final Widget child;
+
+  const AppBackgroundLayout({
+    super.key,
+    this.appBar,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+            Utils.getAssetPNGPath(
+              Utils.isDarkMode(context) ? "bg_dark" : "bg_light",
+            ),
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            if (appBar != null) appBar!,
+            Expanded(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: child,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
