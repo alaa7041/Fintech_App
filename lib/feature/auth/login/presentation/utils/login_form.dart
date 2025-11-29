bb83e45b172eda43bf7ffb71c44c3634b78563f5
alaa <a.magdy2407@gmail.com>
2025-11-29 13:04:50 +0200

    fix password filed🥊

--- lib/feature/auth/login/presentation/utils/login_form.dart
+++ lib/feature/auth/login/presentation/utils/login_form.dart
@@ -18,6 +18,8 @@ class _LoginForm extends StatelessWidget {
           Gap(16),
           AppTextFormField(
             hintText: "Password",
+            isObscureText: true,
+            isPassword: true,
             prefixIcon: Padding(
               padding: EdgeInsets.fromLTRB(24, 16, 12, 16),
               child: Icon(Icons.lock_outline, color: context.colors.primary),
